d5022087816896c7fc8d985ef3c0e693de929eee
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2024-02-08 08:01:33 +0200

    edit

--- app/Http/Controllers/Dashboard/QuestionController.php
+++ app/Http/Controllers/Dashboard/QuestionController.php
@@ -1,131 +1,131 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\Dashboard;
+    namespace App\Http\Controllers\Dashboard;
 
 
-use App\Http\Controllers\Controller;
-use App\Models\Choices;
-use App\Models\Question;
-use App\Models\QuestionBank;
-use App\Models\QuestionHint;
-use App\Models\RevisionQuestionsBank;
-use App\Models\Test;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+    use App\Http\Controllers\Controller;
+    use App\Models\Choices;
+    use App\Models\Question;
+    use App\Models\QuestionBank;
+    use App\Models\QuestionHint;
+    use App\Models\RevisionQuestionsBank;
+    use App\Models\Test;
+    use Illuminate\Database\Eloquent\ModelNotFoundException;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB;
 
-class QuestionController extends Controller
-{
-    //  Questions
-    public function getQuestions()
+    class QuestionController extends Controller
     {
-        $questions = Question::paginate(25);
-        return view('dashboard.question.index')->with("questions", $questions);
-    }
-    public function createQuestion()
-    {
-        $tests = Test::all();
-        return view('dashboard.question.create', compact("tests"));
-    }
-    public function addQuestion(Request $request)
-    {
-        $number = +1;
-        while (Question::where('number', $number)->exists()) {
-            $number++;
+        //  Questions
+        public function getQuestions()
+        {
+            $questions = Question::paginate(25);
+            return view('dashboard.question.index')->with("questions", $questions);
         }
+        public function createQuestion()
+        {
+            $tests = Test::all();
+            return view('dashboard.question.create', compact("tests"));
+        }
+        public function addQuestion(Request $request)
+        {
+            $number = +1;
+            while (Question::where('number', $number)->exists()) {
+                $number++;
+            }
 
-        $data = $request->except(['_token', 'choice_ans', 'choices', 'hint']);
-        $data['number'] = $number;
-        // dd($data);
-        $question = Question::create($data);
-        $question->answer = $request->choice_ans;
-        $question->save();
-        if ($request->type == 1) {
-            foreach ($request->choices as $index => $choice) {
-                $new_choice = new Choices();
-                $new_choice->question_id = $question->id;
-                $new_choice->choice = $choice;
+            $data = $request->except(['_token', 'choice_ans', 'choices', 'hint']);
+            $data['number'] = $number;
+            // dd($data);
+            $question = Question::create($data);
+            $question->answer = $request->choice_ans;
+            $question->save();
+            if ($request->type == 1) {
+                foreach ($request->choices as $index => $choice) {
+                    $new_choice = new Choices();
+                    $new_choice->question_id = $question->id;
+                    $new_choice->choice = $choice;
 
 
 
-                // switch ($request->choice_ans) {
-                //     case 'a':
-                //         if ($index == 0) {
-                //             $new_choice->answer_flag = 1;
-                //         } else
-                //             $new_choice->answer_flag = 0;
-                //         break;
-                //     case 'b':
-                //         if ($index == 1) {
-                //             $new_choice->answer_flag = 1;
-                //         } else
-                //             $new_choice->answer_flag = 0;
-                //         break;
-                //     case 'c':
-                //         if ($index == 2) {
-                //             $new_choice->answer_flag = 1;
-                //         } else
-                //             $new_choice->answer_flag = 0;
-                //         break;
-                //     case 'd':
-                //         if ($index == 3) {
-                //             $new_choice->answer_flag = 1;
-                //         } else
-                //             $new_choice->answer_flag = 0;
-                //         break;
-                // }
+                    // switch ($request->choice_ans) {
+                    //     case 'a':
+                    //         if ($index == 0) {
+                    //             $new_choice->answer_flag = 1;
+                    //         } else
+                    //             $new_choice->answer_flag = 0;
+                    //         break;
+                    //     case 'b':
+                    //         if ($index == 1) {
+                    //             $new_choice->answer_flag = 1;
+                    //         } else
+                    //             $new_choice->answer_flag = 0;
+                    //         break;
+                    //     case 'c':
+                    //         if ($index == 2) {
+                    //             $new_choice->answer_flag = 1;
+                    //         } else
+                    //             $new_choice->answer_flag = 0;
+                    //         break;
+                    //     case 'd':
+                    //         if ($index == 3) {
+                    //             $new_choice->answer_flag = 1;
+                    //         } else
+                    //             $new_choice->answer_flag = 0;
+                    //         break;
+                    // }
 
-                $new_choice->save();
+                    $new_choice->save();
+                }
+            }
+            $questionHint = new QuestionHint();
+            $questionHint->question_id = $question->id;
+            $questionHint->hint = $request->hint;
+            $questionHint->save();
+
+
+            DB::commit();
+
+            return redirect()->back()->with(['success' => __('admin/forms.added_successfully')]);
+        }
+        public function editQuestion($id)
+        {
+            $questions = Question::findOrFail($id);
+            $tests = Test::all();
+            return view('dashboard.question.edit', compact("tests"))->with("questions", $questions);
+        }
+        public function updateQuestion(Request $request, $id)
+        {
+
+            $questions = Question::findOrFail($id);
+            $data = $request->except('_token');
+            $questions->update($data);
+
+            return redirect()->back()->with(['success' => __('admin/forms.updated_successfully')]);
+        }
+        public function deleteQuestion(Request $request, $id)
+        {
+            try {
+                $question = Question::findOrFail($id);
+                $question->delete();
+
+                if ($request->ajax()) {
+                    return response()->json(['success' => __('admin/forms.deleted_successfully')]);
+                } else {
+                    return redirect()->back()->with(['success' => __('admin/forms.deleted_successfully')]);
+                }
+            } catch (ModelNotFoundException $e) {
+                // Handle the case where the record with the given ID does not exist
+                if ($request->ajax()) {
+                    return response()->json(['error' => __('admin/forms.not_found')], 404);
+                } else {
+                    return redirect()->back()->with(['error' => __('admin/forms.not_found')]);
+                }
             }
         }
-        $questionHint = new QuestionHint();
-        $questionHint->question_id = $question->id;
-        $questionHint->hint = $request->hint;
-        $questionHint->save();
+
+        // End Questions
 
 
-        DB::commit();
 
-        return redirect()->back()->with(['success' => __('admin/forms.added_successfully')]);
     }
-    public function editQuestion($id)
-    {
-        $questions = Question::findOrFail($id);
-        $tests = Test::all();
-        return view('dashboard.question.edit', compact("tests"))->with("questions", $questions);
-    }
-    public function updateQuestion(Request $request, $id)
-    {
-
-        $questions = Question::findOrFail($id);
-        $data = $request->except('_token');
-        $questions->update($data);
-
-        return redirect()->back()->with(['success' => __('admin/forms.updated_successfully')]);
-    }
-    public function deleteQuestion(Request $request, $id)
-    {
-        try {
-            $question = Question::findOrFail($id);
-            $question->delete();
-
-            if ($request->ajax()) {
-                return response()->json(['success' => __('admin/forms.deleted_successfully')]);
-            } else {
-                return redirect()->back()->with(['success' => __('admin/forms.deleted_successfully')]);
-            }
-        } catch (ModelNotFoundException $e) {
-            // Handle the case where the record with the given ID does not exist
-            if ($request->ajax()) {
-                return response()->json(['error' => __('admin/forms.not_found')], 404);
-            } else {
-                return redirect()->back()->with(['error' => __('admin/forms.not_found')]);
-            }
-        }
-    }
-
-    // End Questions
-
-
-
-}
